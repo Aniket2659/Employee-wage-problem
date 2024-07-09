03a694431418075e8880e2faf310967fc12b12fa
Aniket2659 <aniket2659@gmail.com>
2024-07-09 12:25:13 +0530

    [aniket] add:script to calculate part time wage

--- emp.py
+++ emp.py
@@ -3,6 +3,11 @@ def check_attendance(attendance):
     if attendance==0:
         return 0
     if attendance==1:
-        return 20*8
+        time=input('enter a type of job :')
+        if time=='part':
+            return 20*6
+        elif time=='full':
+            return 20*8
+
 atd_random=random.randint(0,1)
 print(check_attendance(atd_random))
